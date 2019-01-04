719776713e9553644f570ed1439cb4d1dd224680
Dreizehn133 <Rahasia13>
2019-01-04 09:15:46 +0800

    logins

--- index.php
+++ index.php
@@ -12,84 +12,106 @@
     </style> -->
 </head>
 <body>
-<form action="cari.php" method="POST">
-    <table>
-        <tr><td colspan="2">PENCARIAN</td></tr>
-        <tr>
-            <td>FROM</td>
-            <td><input type="date" name="mulai"/></td>
-        </tr>
-        <tr>
-            <td>TO</td>
-            <td><input type="date" name="akhir"/></td>
-        </tr>
-        <tr>
-            <td>Absen</td>
-            <td><select name="pilihan">
-                <option value="0">Semua</option>
-                <option value="1">Masuk</option>
-                <option value="2">Pulang??</option>
-                <option value="3">Break IN</option>
-                <option value="4">Break OUT</option>
-                <option value="5">Lembur Masuk</option>
-                <option value="6">ot_out/PULANG</option>
-            </select></td>
-        </tr>
-        <tr>
-            <td colspan="2"><input type="submit"></td>
-        </tr>
-    </table>
-</form>
-
-<table  cellpadding="2" border="1">
-
-<tr>
-    <th>First Name</th>
-    <th>Last Name</th>
-    <th>Tanggal Absen</th>
-    <th>Type Attendance</th>
-    <th>keterangan</th>
-    <th>Tanggal Input</th>
-    <th>HAPUS</td>
-</tr>
-
-<?php
-    $x = date('Y-m-d');
-    require_once('koneksi.php');
-    echo "Absen Tanggal ".$x;
-    $sql = "SELECT a.id as id,a.employee_id as eid, a.dt as finger, a.created as serper, b.first_name as nama1, c.name as atd, b.last_name as nama2, a.attendance_type_id as tipe  FROM attendances a, biodata b, attendance_types c WHERE a.employee_id=b.id && a.created LIKE '$x%' && a.attendance_type_id=c.id ORDER BY a.employee_id";
-    $res = mysqli_query($koneksi,$sql);
-  
-  $idd=0;
-  $tipe=0;
-  $ket='';
-    while($row = mysqli_fetch_array($res)){
-        // echo "<tr><td>".$row['nama1']."</td>
-        // <td>".$row['nama2']."</td>
-        // <td>".$row['finger']."</td>
-        // <td>".$row['tipe']."</td>
-        // </tr>";
-        if($row['eid']==$idd && $row['tipe']==$tipe){
-            $ket='Double Absen';
-        }else {
-            $idd=$row['eid'];
-            $tipe=$row['tipe'];
-            $ket='';
-        }
-        ?>
+    <?php 
+        define('X',"YXlhbSBnb3Jlbmc=");
+        define('Y',"NG40SyA0TDR5");
+        
+        session_start();
+        if(!isset($_SESSION['login'])|| $_SESSION['login']!="masuk"){ ?>
+            <form action="index.php" method="POST">
+                <label> USERNAME </label><input type="text" name="usern" >
+                <label> PASSWORD </label><input type="password" name="pws" >
+                <input type="submit" value="Login">
+            </form>
+        <?php }else { ?>
+        <form action="cari.php" method="POST">
+        <table>
+            <tr><td colspan="2">PENCARIAN</td></tr>
             <tr>
-                <td><?=$row['nama1']?></td>
-                <td><?=$row['nama2']?></td>
-                <td><?=$row['finger']?></td>
-                <td><?=$row['tipe']?> - <?=$row['atd']?></td>
-                <td><?=$ket?></td>
-                <td><?=$row['serper']?></td>
-                <td><a href="delete.php?q=<?=$row['id']?>" >Hapus</a></td>
+                <td>FROM</td>
+                <td><input type="date" name="mulai"/></td>
             </tr>
-        <?php
-    }
-?>
-</table>
+            <tr>
+                <td>TO</td>
+                <td><input type="date" name="akhir"/></td>
+            </tr>
+            <tr>
+                <td>Absen</td>
+                <td><select name="pilihan">
+                    <option value="0">Semua</option>
+                    <option value="1">Masuk</option>
+                    <option value="2">Pulang??</option>
+                    <option value="3">Break IN</option>
+                    <option value="4">Break OUT</option>
+                    <option value="5">Lembur Masuk</option>
+                    <option value="6">ot_out/PULANG</option>
+                </select></td>
+            </tr>
+            <tr>
+                <td colspan="2"><input type="submit"></td>
+            </tr>
+        </table>
+    </form>
+    
+    <table  cellpadding="2" border="1">
+    
+    <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Tanggal Absen</th>
+        <th>Type Attendance</th>
+        <th>keterangan</th>
+        <th>Tanggal Input</th>
+        <th>HAPUS</td>
+    </tr>
+    
+    <?php
+        $x = date('Y-m-d');
+        require_once('koneksi.php');
+        echo "Absen Tanggal ".$x;
+        $sql = "SELECT a.id as id,a.employee_id as eid, a.dt as finger, a.created as serper, b.first_name as nama1, c.name as atd, b.last_name as nama2, a.attendance_type_id as tipe  FROM attendances a, biodata b, attendance_types c WHERE a.employee_id=b.id && a.created LIKE '$x%' && a.attendance_type_id=c.id ORDER BY a.employee_id";
+        $res = mysqli_query($koneksi,$sql);
+      
+      $idd=0;
+      $tipe=0;
+      $ket='';
+        while($row = mysqli_fetch_array($res)){
+            // echo "<tr><td>".$row['nama1']."</td>
+            // <td>".$row['nama2']."</td>
+            // <td>".$row['finger']."</td>
+            // <td>".$row['tipe']."</td>
+            // </tr>";
+            if($row['eid']==$idd && $row['tipe']==$tipe){
+                $ket='Double Absen';
+            }else {
+                $idd=$row['eid'];
+                $tipe=$row['tipe'];
+                $ket='';
+            }
+            ?>
+                <tr>
+                    <td><?=$row['nama1']?></td>
+                    <td><?=$row['nama2']?></td>
+                    <td><?=$row['finger']?></td>
+                    <td><?=$row['tipe']?> - <?=$row['atd']?></td>
+                    <td><?=$ket?></td>
+                    <td><?=$row['serper']?></td>
+                    <td><a href="delete.php?q=<?=$row['id']?>" >Hapus</a></td>
+                </tr>
+            <?php
+        }
+    ?>
+    </table>
+    <?php
+        }
+        if($_SERVER['REQUEST_METHOD']=='POST') {
+            $a = base64_encode($_POST['usern']);
+            $b = base64_encode($_POST['pws']);
+            if($a==X && $b==Y){
+                $_SESSION['login']="masuk";
+            }
+        }
+    ?>
 </body>
 </html>
 
---
+++ tyes.php
@@ -0,0 +1,7 @@
+<?php
+echo md5("ayam goreng");
+echo "<br>";
+echo base64_encode("ayam goreng");
+echo "<br>";
+echo base64_encode("4n4K 4L4y");
+?>
